dd707ba385133c1e188134b9628bb5c5eac7c85c
albertotn <amartinelli@fbk.eu>
2016-06-30 14:38:50 +0200

    updated hello world example

--- src/main/java/example/helloworld/HelloWorldJUnitTest.java
+++ src/main/java/example/helloworld/HelloWorldJUnitTest.java
@@ -2,28 +2,18 @@ package example.helloworld;
 
 import org.jbpm.test.JbpmJUnitBaseTestCase;
 import org.junit.Test;
-import org.kie.api.KieBase;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.process.ProcessInstance;
-import org.kie.internal.io.ResourceFactory;
-import org.kie.internal.utils.KieHelper;
 
 public class HelloWorldJUnitTest extends JbpmJUnitBaseTestCase {
 
 	@Test
-	public void testImplicit() {
-		KieHelper kieHelper = new KieHelper();
-		KieBase kieBase = kieHelper.addResource(ResourceFactory.newClassPathResource("HelloWorld.bpmn2")).build();
-
-		KieSession ksession = kieBase.newKieSession();
-
+	public void helloWorld() {
+		KieSession ksession = createRuntimeManager("example/helloworld/HelloWorld.bpmn2").getRuntimeEngine(null)
+				.getKieSession();
 		ProcessInstance processInstance = ksession.startProcess("example.helloworld.HelloWorld");
-		// do your checks here
-		// for example, assertProcessInstanceCompleted(processInstance.getId(),
-		// ksession);
-		System.out.println("id: " + processInstance.getId());
-		System.out.println("parent id: " + processInstance.getParentProcessInstanceId());
 
+		assertTrue(processInstance != null);
 	}
 
 }
